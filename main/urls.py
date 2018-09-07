6f65fadef3ddd9d6bd5150dfdc679e0315d11b60
ArpanMangal <mangalarpan@gmail.com>
2018-09-08 02:40:08 +0530

    Updated API

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path('api/getDepartmentStudentRecords', views.getDepartmentStudentRecords, name='getDepartmentStudentRecords'),
+    path('api/getAllDepartmentRecords', views.getAllDepartmentRecords, name='getAllDepartmentRecords')
 
 ]
