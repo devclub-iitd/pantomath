c6b36c4f77657e848167c2bde579302d80225b9f
Arshdeep Singh <arshdeepiitd@gmail.com>
2018-09-09 23:48:42 +0530

    updated views and requirements

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # this saves a students records json file on server
     path('save_student_data/', views.save_student_data, name='index'),
-    
+    path('api/registeredCourses', views.registered_courses, name='registered'),
+        
     path('api/getDepartmentStudentRecords', views.getDepartmentStudentRecords, name='getDepartmentStudentRecords'),
     path('api/getAllDepartmentRecords', views.getAllDepartmentRecords, name='getAllDepartmentRecords')
 
